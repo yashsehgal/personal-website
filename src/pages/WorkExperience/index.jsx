921c1b46b6528affecbfbbfd5b29be9c9c81ba2e
Yash Sehgal Clamp <yash.sehgal@joinclamp.com>
2023-02-28 17:06:11 +0530

    feat: clamp work experience added

--- src/pages/WorkExperience/index.jsx
+++ src/pages/WorkExperience/index.jsx
@@ -9,7 +9,7 @@ export default function WorkExperience() {
             <div className="work-experience-content-wrapper mt-20">
                 <h1 className="experience-title leading-snug text-2xl font-semibold text-gray-800">Work Experience</h1>
                 <div className="work-experience-list-wrapper mt-6">
-                    <ul className="work-experience-list flex flex-row items-start justify-start gap-3">
+                    <ul className="work-experience-list grid grid-cols-2 items-start justify-start gap-3">
                         {workExperienceDataRef.current?.map((workExperience, workExperienceIndex) => (
                             <li className="work-experience-item"
                                 key={workExperienceIndex}
@@ -19,9 +19,11 @@ export default function WorkExperience() {
                                 >
                                     <div className="w-fit h-fit flex flex-col items-start justify-start gap-1">
                                         {workExperience?.coverImage
-                                            ? <div className="work-experience-comapny-cover-image mb-1 bg-white rounded w-[420px] h-[200px] flex flex-row items-center justify-center border border-gray-200">
+                                            ? <div className="work-experience-comapny-cover-image mb-1 bg-white rounded w-[420px] h-[200px] flex flex-row items-center justify-center border border-gray-200 overflow-hidden">
                                                 <img src={workExperience?.coverImage}
-                                                    className="max-w-[200px] max-h-[100px]"
+                                                    className={workExperience?.company?.toLowerCase() === "clamp"
+                                                        || "max-w-[200px] max-h-[100px]"
+                                                    }
                                                     alt="company-cover"
                                                 />
                                             </div>
