ee7a933cea29c5da837afda605a1fe2832a6b96a
Yash Sehgal <yashsehgal.work@gmail.com>
2023-03-24 07:54:30 +0530

    feat: resume view added

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,19 +4,20 @@ import ExperienceSection from '@/components/main/ExperienceSection';
 import HeroSection from '@/components/main/HeroSection';
 import ProjectSection from '@/components/main/ProjectSection';
 import MetaHead from '@/components/seo/MetaHead';
-import LinkText from '@/components/ui/LinkText';
 
 const MainView: React.FunctionComponent = () => {
   return (
-    <div className="main-view-container">
+    <>
       <MetaHead />
-      <ViewContainer className="grid grid-cols-1 items-start justify-start gap-12">
-        <HeroSection />
-        <ExperienceSection />
-        <ProjectSection />
-        <ContactSection />
-      </ViewContainer>
-    </div>
+      <div className="main-view-container">
+        <ViewContainer className="grid grid-cols-1 items-start justify-start gap-12">
+          <HeroSection />
+          <ExperienceSection />
+          <ProjectSection />
+          <ContactSection />
+        </ViewContainer>
+      </div>
+    </>
   );
 };
 
