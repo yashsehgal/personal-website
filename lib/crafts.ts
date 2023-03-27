ce33d05feb458584946d2bdbe5c10d75af8042e0
Yash Sehgal <yashsehgal.work@gmail.com>
2023-03-27 16:47:12 +0530

    feat(wip): crafts data added

---
+++ lib/crafts.ts
@@ -0,0 +1,13 @@
+import { CraftProps } from '@/pages/crafts';
+
+export const getCrafts = function (): CraftProps[] {
+  return [
+    {
+      craftTitle: 'Types and Interfaces for React Components',
+      craftDescription:
+        'Beautiful Button Component with Typescript + React + TailwindCSS',
+      linkType: 'internal',
+      craftSlug: 'typescript-react-button-component',
+    },
+  ];
+};
--- pages/crafts/index.tsx
+++ pages/crafts/index.tsx
@@ -1,4 +1,15 @@
+import Section from '@/components/layout/Section';
+import ViewContainer from '@/components/layout/ViewContainer';
 import MetaHead from '@/components/seo/MetaHead';
+
+type CraftLinkType = 'internal' | 'external';
+interface CraftProps {
+  craftTitle?: string;
+  linkType?: CraftLinkType;
+  craftDescription?: string | React.ReactNode;
+  craftSlug?: string;
+  craftLink?: string;
+}
 
 const Crafts: React.FunctionComponent = () => {
   return (
@@ -14,8 +25,24 @@ const Crafts: React.FunctionComponent = () => {
           og: 'https://ik.imagekit.io/eawrckp8wfi/portfolio-image-source/seo/crafts?updatedAt=1679904848893',
         }}
       />
+      <div className="crafts-view-container" id="crafts">
+        <ViewContainer>
+          <Section
+            className="crafts-list-wrapper mt-8"
+            id="crafts-list"
+            aria-label="Crafts">
+            <h2 className="about-heading leading-snug font-medium text-base text-zinc-900">
+              {'crafts.'}
+            </h2>
+            <ul className="crafts-list mt-8"></ul>
+          </Section>
+        </ViewContainer>
+      </div>
     </>
   );
 };
 
 export default Crafts;
+export type { CraftProps };
+
+// React.useMemo(() => (async () => await myLongAsyncFunction(args)), [args])
