bae1e28e97c4c6ef3c7af4c1450e7fe391b246d1
Yash Sehgal <yashsehgal.work@gmail.com>
2025-10-31 13:58:16 +0530

    cleaner

--- app/writing/ios-like-app-drawer/_components/app-drawer-trigger.tsx
+++ app/writing/ios-like-app-drawer/_components/app-drawer-trigger.tsx
@@ -28,8 +28,8 @@ export function AppDrawerTrigger({ setIsOpen }: AppDrawerTriggerProps) {
       className="flex items-center gap-0 cursor-pointer focus-visible:outline-none"
       whileHover={{ scale: 1.06 }}
       transition={{ type: 'spring', duration: 0.6 }}>
-      <div className="w-6 h-6 rounded-md border border-white/10 -z-20 bg-transparent backdrop-blur-lg" />
-      <div className="w-8 h-8 rounded-lg border border-white/20 -z-10 -ml-5 bg-transparent backdrop-blur-lg" />
+      <div className="w-6 h-6 rounded-md border-y border-l border-white/10 -z-20 bg-transparent" />
+      <div className="w-8 h-8 rounded-lg border-y border-l border-white/20 -z-10 -ml-5 bg-transparent" />
       <div className="w-10 h-10 rounded-xl border border-white/20 z-0 bg-transparent -ml-7 backdrop-blur-3xl">
         <div className="grid grid-cols-2 items-center justify-center w-full h-full scale-[85%] pl-[3px]">
           {APP_DRAWER_TRIGGER_ICON_PATHNAMES.map((app, index) => {
