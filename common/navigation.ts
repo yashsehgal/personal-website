a5fffc8333b4c976f788c9a0afeed5baa98ef2f0
Yash Sehgal <yashsehgal.work@gmail.com>
2026-01-24 01:50:59 +0530

    Refactor navigation structure to use a title map for improved maintainability and simplify getRouteTitle function

--- common/navigation.ts
+++ common/navigation.ts
@@ -1,21 +1,22 @@
 import { ApplicationRoute, ROUTES } from '@/common/routes';
 
+export const NAVIGATION_SEGMENT_TITLE_MAP = {
+  [ROUTES.HOME]: 'Home',
+  [ROUTES.WORK]: 'Work',
+  [ROUTES.POSTS]: 'Posts',
+} as const;
+
 export const NAVIGATION: { title: string; route: ApplicationRoute }[] = [
-  { title: 'Home', route: ROUTES.HOME },
-  { title: 'Work', route: ROUTES.WORK },
-  { title: 'Posts', route: ROUTES.POSTS },
+  { title: NAVIGATION_SEGMENT_TITLE_MAP[ROUTES.HOME], route: ROUTES.HOME },
+  { title: NAVIGATION_SEGMENT_TITLE_MAP[ROUTES.WORK], route: ROUTES.WORK },
+  { title: NAVIGATION_SEGMENT_TITLE_MAP[ROUTES.POSTS], route: ROUTES.POSTS },
 ] as const;
 
-export const getRouteTitle = (route: ApplicationRoute): string => {
+export const getRouteTitle = (route: string): string => {
   const routeWithInitialSlash: string = '/' + route;
-  switch (routeWithInitialSlash) {
-    case ROUTES.HOME:
-      return 'Home';
-    case ROUTES.WORK:
-      return 'Work';
-    case ROUTES.POSTS:
-      return 'Posts';
-    default:
-      return '';
-  }
+  return (
+    NAVIGATION_SEGMENT_TITLE_MAP[
+      routeWithInitialSlash as keyof typeof NAVIGATION_SEGMENT_TITLE_MAP
+    ] ?? ''
+  );
 };
