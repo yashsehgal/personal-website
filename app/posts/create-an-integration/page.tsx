7529ff0bf91c2239bedbb1c8027ce4f25e28b568
Yash Sehgal <yashsehgal.work@gmail.com>
2026-01-24 08:21:09 +0530

    Update integration setup UI: adjust width of the integration manager container from 'w-lg' to 'w-md' for improved layout consistency.

--- app/posts/create-an-integration/page.tsx
+++ app/posts/create-an-integration/page.tsx
@@ -3,7 +3,7 @@ import { IntegrationManager } from '@/app/posts/create-an-integration/components
 export default function PostCreateAnIntegrationPage() {
   return (
     <div className="p-6 space-y-8 h-full flex items-center justify-center">
-      <div className="relative h-[420px] border w-lg border-neutral-200 rounded-3xl">
+      <div className="relative h-[420px] border w-md border-neutral-200 rounded-3xl">
         <IntegrationManager />
       </div>
     </div>
