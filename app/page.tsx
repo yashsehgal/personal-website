37471e8d622743fe4971115267da93ad1cb05888
Yash Sehgal <yashsehgal.work@gmail.com>
2026-01-27 12:02:52 +0530

    work items, work page section fixes

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Page() {
   return (
     <div className="home-page space-y-16">
       <h1 className="text-2xl font-semibold ml-1.5">Hi, I am Yash</h1>
-      <div className="grid grid-cols-2 items-start justify-start w-fit gap-12 max-lg:flex max-lg:flex-col">
+      <div className="grid grid-cols-2 items-start justify-start w-fit gap-24 max-lg:flex max-lg:flex-col max-xl:gap-12">
         <div className="post-container w-72 flex flex-col items-start gap-12">
           <h2 className="text-xl font-semibold ml-1.5">Posts</h2>
           <div className="post-list-container gap-8 flex items-start flex-col">
--- components/link-button.tsx
+++ components/link-button.tsx
@@ -22,7 +22,8 @@ export function LinkButton({
       className={cn(
         'button cursor-pointer flex items-center',
         variant === 'default' && 'text-foreground hover:bg-foreground/10',
-        size === 'default' && 'py-0.5 px-1.5 rounded-md font-semibold gap-1.5',
+        size === 'default' &&
+          'py-0.5 px-1.5 rounded-md font-semibold gap-1.5 hover:text-foreground',
         size === 'sm' && '',
         size === 'lg' && '',
         className,
