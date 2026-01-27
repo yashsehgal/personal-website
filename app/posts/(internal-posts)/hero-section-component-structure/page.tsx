174d21646e794641ed806f24323a046496a2dc24
Yash Sehgal <yashsehgal.work@gmail.com>
2026-01-27 14:30:59 +0530

    internal post page cleanup

--- app/posts/(internal-posts)/hero-section-component-structure/page.tsx
+++ app/posts/(internal-posts)/hero-section-component-structure/page.tsx
@@ -1,5 +1,17 @@
 import { InternalPostContainer } from '@/components/sections/internal-post-container';
 
 export default function HeroSectionComponentStructurePage() {
-  return <InternalPostContainer></InternalPostContainer>;
+  return (
+    <InternalPostContainer>
+      <div className="">
+        <p className="text">
+          This hero section is the most simple and commonly used pattern for
+          both marketing-led landing pages and data-intensive dashboard pages.
+          Consider an example for showing templates inside a writing platform
+          that has a hero section on top. In real life, the section can also be
+          reused on the marketing page for showing templates.
+        </p>
+      </div>
+    </InternalPostContainer>
+  );
 }
--- components/sections/internal-post-container.tsx
+++ components/sections/internal-post-container.tsx
@@ -1,7 +1,9 @@
 'use client';
-import { ApplicationRoute } from '@/common/route';
+import { ApplicationRoute, ROUTES } from '@/common/route';
+import { LinkButton } from '@/components/link-button';
 import { getInternalPostData } from '@/constants/interal-posts';
 import { cn } from '@/helpers/cn';
+import { IconArrowLeft } from '@tabler/icons-react';
 import { usePathname } from 'next/navigation';
 
 type InternalPostContainerProps = React.HTMLAttributes<HTMLDivElement>;
@@ -12,18 +14,30 @@ export function InternalPostContainer({
   ...props
 }: InternalPostContainerProps) {
   const pathname = usePathname();
-
   const internalPostTitle: string =
     getInternalPostData(pathname as unknown as ApplicationRoute)?.title ?? '';
 
   return (
     <div
-      className={cn('internal-post-container w-3xl mx-auto', className)}
+      className={cn(
+        'internal-post-container w-3xl mx-auto space-y-12',
+        className,
+      )}
       {...props}>
+      <div className="flex items-center justify-between gap-4">
+        <LinkButton
+          href={ROUTES.POSTS}
+          className="w-fit text-secondary hover:text-foreground">
+          <IconArrowLeft size={16} />
+          <span>Back</span>
+        </LinkButton>
+      </div>
       <header className="internal-post-container-header">
         <h1 className="font-semibold">{internalPostTitle}</h1>
       </header>
-      <main className="internal-post-container-main-content">{children}</main>
+      <main className="internal-post-container-main-content [&_.text]:leading-7">
+        {children}
+      </main>
     </div>
   );
 }
