11e0634d07270d619a96de74697ed8a39a6c1510
Yash Sehgal <yashsehgal.work@gmail.com>
2026-01-28 09:30:31 +0530

    add links to link buttons

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,9 @@ export default function Page() {
       <h1 className="text-2xl font-semibold ml-1.5">Hi, I am Yash</h1>
       <div className="grid grid-cols-2 items-start justify-start w-fit gap-24 max-lg:flex max-lg:flex-col max-xl:gap-12">
         <div className="post-container w-72 flex flex-col items-start gap-12">
-          <Button size="lg">Posts</Button>
+          <LinkButton size="lg" href={ROUTES.POSTS}>
+            Posts
+          </LinkButton>
           <div className="post-list-container gap-8 flex items-start flex-col">
             {slicedPostItems.map((post, index) => {
               return (
@@ -54,7 +56,9 @@ export default function Page() {
           </div>
         </div>
         <div className="work-and-projects--container w-72 flex flex-col items-start gap-12">
-          <Button size="lg">Work and projects</Button>
+          <LinkButton size="lg" href={ROUTES.WORK}>
+            Work and projects
+          </LinkButton>
           <div className="work-and-projects-list-container gap-8 flex items-start flex-col">
             {slicedWorkItems.map((work, index) => {
               return (
