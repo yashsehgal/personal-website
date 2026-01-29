bdb3d8b3916f3f9cf699b869fc13d3526d7e48ba
Yash Sehgal <yashsehgal.work@gmail.com>
2026-01-29 17:00:34 +0530

    hmmmmmm

--- app/posts/(internal-posts)/breadcrumb-component/page.tsx
+++ app/posts/(internal-posts)/breadcrumb-component/page.tsx
@@ -2,9 +2,20 @@
 import { DIRECTORY } from '@/app/posts/(internal-posts)/breadcrumb-component/constants';
 import { ComponentPreviewContainer } from '@/components/component-preview-container';
 import { InternalPostContainer } from '@/components/sections/internal-post-container';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import { cn } from '@/helpers/cn';
 import {
+  IconArrowDownLeft,
+  IconArrowDownRight,
+  IconArrowForward,
   IconChevronRight,
+  IconCornerDownRight,
+  IconDots,
   IconFolderFilled,
   IconPointFilled,
 } from '@tabler/icons-react';
@@ -14,6 +25,7 @@ import { useState } from 'react';
 
 const DASHBOARD_SIDEBAR_TREE_NODE_ICON: number = 16 as const;
 const NUQS_SELECTED_NODE_ITEM: string = 'nodeId';
+const BASE_SEGMENTS_LIMIT: number = 3 as const;
 
 export interface Directory {
   id: string;
@@ -35,7 +47,92 @@ function DashboardComponentPreview() {
   return (
     <div className="w-full h-full flex items-stretch justify-start bg-foreground/5 p-2 gap-2">
       <DashboardSidebarComponent />
-      <main className="flex-1 bg-background rounded-xl shadow-2xs border border-foreground/10"></main>
+      <main className="flex-1 bg-background rounded-xl shadow-2xs border border-foreground/10 px-4">
+        <DashboardBreadcrumbComponent />
+      </main>
+    </div>
+  );
+}
+
+function DashboardBreadcrumbComponent() {
+  const [selectedFileNodeId] = useQueryState(NUQS_SELECTED_NODE_ITEM);
+  const segments = selectedFileNodeId?.split('/');
+
+  const [middleSegmentsDropdownOpen, setMiddleSegmentsDropdownOpen] =
+    useState<boolean>(false);
+
+  const handleMiddleSegmentsDropdownOpenStateChange = (open: boolean) => {
+    setMiddleSegmentsDropdownOpen(open);
+  };
+
+  const getMiddleSegments = () => {
+    if (!segments || segments.length <= 1) return null;
+    return segments.slice(0, -1);
+  };
+
+  const shouldTruncateSegments: boolean = segments
+    ? segments?.length > BASE_SEGMENTS_LIMIT
+    : false;
+
+  return (
+    <div className="flex items-center justify-start gap-2 text-foreground font-medium h-12">
+      <p className="text-sm">Home</p>
+      {shouldTruncateSegments ? (
+        <>
+          <IconChevronRight
+            size={DASHBOARD_SIDEBAR_TREE_NODE_ICON}
+            className="shrink-0"
+          />
+          <DropdownMenu
+            open={middleSegmentsDropdownOpen}
+            onOpenChange={handleMiddleSegmentsDropdownOpenStateChange}>
+            <DropdownMenuTrigger asChild>
+              <button className="size-7 rounded-lg flex items-center justify-center hover:bg-foreground/10 cursor-pointer active:bg-foreground/15">
+                <IconDots size={DASHBOARD_SIDEBAR_TREE_NODE_ICON} />
+              </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              className="min-w-56 max-w-80 rounded-xl"
+              align="start"
+              alignOffset={-36}>
+              {getMiddleSegments()?.map((middleSegment, index) => {
+                return (
+                  <DropdownMenuItem
+                    key={middleSegment}
+                    className="first:rounded-t-lg last:rounded-b-lg cursor-pointer font-medium"
+                    style={{ paddingLeft: `${(index + 1) * 12}px` }}>
+                    <IconCornerDownRight
+                      size={DASHBOARD_SIDEBAR_TREE_NODE_ICON}
+                    />
+                    {middleSegment}
+                  </DropdownMenuItem>
+                );
+              })}
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <IconChevronRight
+            size={DASHBOARD_SIDEBAR_TREE_NODE_ICON}
+            className="shrink-0"
+          />
+          {segments ? (
+            <p className="text-sm">{segments[segments.length - 1]}</p>
+          ) : null}
+        </>
+      ) : (
+        segments?.map((segment, index) => {
+          return (
+            <>
+              <IconChevronRight
+                size={DASHBOARD_SIDEBAR_TREE_NODE_ICON}
+                className="shrink-0"
+              />
+              <p key={index} className="text-sm">
+                {segment}
+              </p>
+            </>
+          );
+        })
+      )}
     </div>
   );
 }
@@ -63,10 +160,9 @@ function DashboardSidebarTreeNodeContainerComponent({
   node: Directory;
   level: number;
 }) {
-  const [selectedFileNodeId, setSelectedFileNodeId] = useQueryState(
-    NUQS_SELECTED_NODE_ITEM,
-    { defaultValue: '' },
-  );
+  const [_, setSelectedFileNodeId] = useQueryState(NUQS_SELECTED_NODE_ITEM, {
+    defaultValue: '',
+  });
   const [openFolderNode, setOpenFolderNode] = useState<boolean>(false);
   const isNodeFolder: boolean = node.children.length > 0;
 
@@ -94,7 +190,6 @@ function DashboardSidebarTreeNodeContainerComponent({
             <motion.div
               key={node.id}
               className="dashboard-sidebar-tree-node-children-items-container overflow-hidden"
-              layout="size"
               initial={{ height: 0 }}
               animate={{ height: 'fit-content' }}
               exit={{ height: 0 }}
@@ -134,10 +229,18 @@ type DashboardSidebarNodeComponentProps = Omit<Directory, 'children'> & {
 function DashboardSidebarNodeComponent(
   node: DashboardSidebarNodeComponentProps,
 ) {
+  const [selectedFileNodeId] = useQueryState(NUQS_SELECTED_NODE_ITEM);
+
+  const isNodeSelected: boolean = selectedFileNodeId === node.id;
+
   return (
     <button
       onClick={node.onClick}
-      className="w-full h-8 hover:bg-foreground/5 flex items-center justify-start gap-2 rounded-md cursor-pointer group/tree-node pr-3 active:bg-foreground/10"
+      className={cn(
+        'w-full h-8 hover:text-foreground flex items-center justify-start gap-2 rounded-md cursor-pointer group/tree-node pr-3',
+        isNodeSelected &&
+          'bg-foreground/5 hover:bg-foreground/5 active:bg-foreground/5',
+      )}
       style={{
         paddingLeft: `${(node.isNodeFolder ? 12 : 12) * node.level}px`,
       }}>
@@ -152,16 +255,23 @@ function DashboardSidebarNodeComponent(
           />
           <IconFolderFilled
             size={DASHBOARD_SIDEBAR_TREE_NODE_ICON}
-            className="text-blue-300 group-hover/tree-node:text-blue-400 shrink-0 dark:text-blue-400"
+            className="text-blue-400 shrink-0"
           />
         </>
       ) : (
         <IconPointFilled
           size={DASHBOARD_SIDEBAR_TREE_NODE_ICON}
-          className="text-foreground/50 shrink-0 group-hover/tree-node:text-foreground"
+          className={cn(
+            'text-foreground/50 shrink-0 group-hover/tree-node:text-foreground',
+            isNodeSelected && 'text-foreground',
+          )}
         />
       )}
-      <p className="text-sm font-medium text-secondary truncate group-hover/tree-node:text-foreground">
+      <p
+        className={cn(
+          'text-sm font-medium text-secondary truncate group-hover/tree-node:text-foreground',
+          isNodeSelected && 'text-foreground',
+        )}>
         {node.name}
       </p>
     </button>
