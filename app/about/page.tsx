e1f504a3e0f5e2109d6c79719021c34b1e75534a
Yash Sehgal <yashsehgal.work@gmail.com>
2026-02-04 12:48:13 +0530

    add about content on home page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,39 +11,42 @@ export default function AboutPage() {
   return (
     <div className="about-page space-y-16 pt-8 max-lg:pt-0">
       <h1 className="text-xl font-semibold ml-1.5">Hi,</h1>
-      <motion.div
-        key="profile-image-container"
-        initial={{ filter: 'blur(6px)', opacity: 0.1 }}
-        animate={{ filter: 'blur(0px)', opacity: 1 }}
-        transition={{
-          duration: 0.8,
-          delay: 0.3,
-          type: 'spring',
-          bounce: 0,
-          ease: 'easeInOut',
-        }}>
-        <Image
-          src={PROFILE_IMAGE_URL}
-          alt="Profile image"
-          width={600}
-          height={600}
-          priority
-          className="size-42 object-cover object-center ml-1.5 pointer-events-none cursor-default select-none"
-        />
-      </motion.div>
-      <div className="space-y-6">
-        <p className="text-foreground font-medium text-2xl w-4/5 leading-9 max-lg:text-lg max-md:text-base max-lg:leading-8 max-md:leading-6">
+      <div className="size-42">
+        <motion.div
+          key="profile-image-container"
+          className="size-42 overflow-hidden"
+          initial={{ filter: 'blur(8px)', opacity: 0.1, y: 24, height: 0 }}
+          animate={{ filter: 'blur(0px)', opacity: 1, y: 0, height: 'auto' }}
+          transition={{
+            duration: 0.8,
+            delay: 0.3,
+            type: 'spring',
+            bounce: 0,
+            ease: 'easeInOut',
+          }}>
+          <Image
+            src={PROFILE_IMAGE_URL}
+            alt="Profile image"
+            width={600}
+            height={600}
+            priority
+            className="size-42 object-cover object-center ml-1.5 pointer-events-none cursor-default select-none"
+          />
+        </motion.div>
+      </div>
+      <div className="space-y-6 ml-1.5">
+        <motion.p className="text-foreground font-medium text-2xl w-4/5 leading-9 max-lg:text-lg max-md:text-base max-lg:leading-8 max-md:leading-6">
           I am design engineer based out of India. My recent work experience
           includes designing dashboards, workflow builders, improving user
           experiences of different AI agent tools, and implementing design
           systems.
-        </p>
-        <p className="text-foreground font-medium text-2xl w-4/5 leading-9 text-balance max-lg:text-lg max-md:text-base max-lg:leading-8 max-md:leading-6">
+        </motion.p>
+        <motion.p className="text-foreground font-medium text-2xl w-4/5 leading-9 text-balance max-lg:text-lg max-md:text-base max-lg:leading-8 max-md:leading-6">
           I focus on accessible, usable experiences, with a balanced layout,
           clear typography, and micro-interactions. I build UI with TypeScript
           and React and handle animations with framer-motion.
-        </p>
-        <p className="text-foreground font-medium text-2xl w-4/5 leading-9 max-lg:text-lg max-md:text-base max-lg:leading-8 max-md:leading-6">
+        </motion.p>
+        <motion.p className="text-foreground font-medium text-2xl w-4/5 leading-9 max-lg:text-lg max-md:text-base max-lg:leading-8 max-md:leading-6">
           Besides work, I love listening to music and playing and making music.
           I play piano and make background scores for storylines in my free
           time.
@@ -56,7 +59,7 @@ export default function AboutPage() {
               className="shrink-0 max-lg:size-4 max-md:size-4"
             />
           </LinkButton>
-        </p>
+        </motion.p>
       </div>
     </div>
   );
