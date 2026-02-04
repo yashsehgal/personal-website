d73dd2ceadc4de31e155fba27051301f16bed738
Yash Sehgal <yashsehgal.work@gmail.com>
2026-02-05 00:54:32 +0530

    fixes

---
+++ constants/socials.ts
@@ -0,0 +1,6 @@
+export const SOCIALS = {
+  X: 'https://x.com/yashsehgaldev',
+  LINKEDIN: 'https://www.linkedin.com/in/sehgalyash/',
+  GITHUB: 'https://github.com/yashsehgal',
+  MAIL: 'yashsehgal.work@gmail.com',
+} as const;
