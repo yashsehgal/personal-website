f632a637e1e2c18c08333c628ede0fa5eaf9e7ca
Yash Sehgal <yashsehgal.work@gmail.com>
2026-02-17 04:25:04 +0530

    Improve code for rendering images and captions

--- app/posts/(internal-posts)/photography/page.tsx
+++ app/posts/(internal-posts)/photography/page.tsx
@@ -1,36 +1,55 @@
 import { InternalPostContainer } from '@/components/sections/internal-post-container';
 import Image from 'next/image';
 
-const PHOTOGRAPHY_IMAGES: number = 10 as const;
-
-const PHOTOGRAPHY_IMAGES_CAPTIONS: string[] = [
-  'Disneyland, Hong Kong',
-  'Palace of Fine Arts, San Francisco, California',
-  'Somewhere near to the Palace of Fine Arts, San Francisco, California',
-  'Palace of Fine Arts, San Francisco, California',
-  'Sunset at the Golden Gate Bridge, San Francisco, California',
-  'Golden Gate Bridge, San Francisco, California',
-  'Near to Alcatraz Island, San Francisco, California',
-  'A Boat Club, San Francisco, California',
-  'Petronas Twin Towers, Kuala Lumpur, Malaysia',
-  'Late-evening view of the Island, Hong Kong',
+const PHOTOGRAPHY_IMAGES: { path: string; caption: string }[] = [
+  { path: '1.JPG', caption: 'Disneyland, Hong Kong' },
+  { path: '2.JPG', caption: 'Palace of Fine Arts, San Francisco, California' },
+  {
+    path: '3.JPG',
+    caption:
+      'Somewhere near to the Palace of Fine Arts, San Francisco, California',
+  },
+  { path: '4.JPG', caption: 'Palace of Fine Arts, San Francisco, California' },
+  {
+    path: '5.JPG',
+    caption: 'Sunset at the Golden Gate Bridge, San Francisco, California',
+  },
+  {
+    path: '6.JPG',
+    caption: 'Golden Gate Bridge, San Francisco, California',
+  },
+  {
+    path: '7.JPG',
+    caption: 'Near to Alcatraz Island, San Francisco, California',
+  },
+  { path: '8.JPG', caption: 'A Boat Club, San Francisco, California' },
+  {
+    path: '9.JPG',
+    caption: 'Petronas Twin Towers, Kuala Lumpur, Malaysia',
+  },
+  {
+    path: '10.JPG',
+    caption: 'Late-evening view of the Island, Hong Kong',
+  },
 ];
 
 export default function PhotographyPage() {
   return (
     <InternalPostContainer>
       <div className="grid grid-cols-1 gap-12 pb-12">
-        {Array.from({ length: PHOTOGRAPHY_IMAGES }).map((_, index) => (
-          <div key={index} className="flex flex-col gap-4 items-center w-full">
+        {PHOTOGRAPHY_IMAGES.map((item, index) => (
+          <div
+            key={item.path}
+            className="flex flex-col gap-4 items-center w-full">
             <Image
-              src={`/photography/${index + 1}.JPG`}
-              alt={`Photography ${index + 1}`}
+              src={`/photography/${item.path}`}
+              alt={item.caption}
               width={1200}
               height={800}
               className="select-none pointer-events-none w-full h-full object-cover"
             />
             <p className="text-sm text-center font-medium">
-              {`${index + 1}. ${PHOTOGRAPHY_IMAGES_CAPTIONS[index]}`}
+              {`${index + 1}. ${item.caption}`}
             </p>
           </div>
         ))}
