977ebdd9b4194d7193a22b21a5d21641fa4b03e1
Yash Sehgal <yashsehgal.work@gmail.com>
2026-02-17 04:22:02 +0530

    Fix spacing and captions

--- app/posts/(internal-posts)/photography/page.tsx
+++ app/posts/(internal-posts)/photography/page.tsx
@@ -5,29 +5,29 @@ const PHOTOGRAPHY_IMAGES: number = 10 as const;
 
 const PHOTOGRAPHY_IMAGES_CAPTIONS: string[] = [
   'Disneyland, Hong Kong',
-  'Golden Gate Bridge, San Francisco, California',
   'Palace of Fine Arts, San Francisco, California',
-  'Alcatraz Island, San Francisco, California',
-  'Somewhere near to Palace of Fine Arts, San Francisco, California',
-  'A Boat Club, San Francisco, California',
+  'Somewhere near to the Palace of Fine Arts, San Francisco, California',
   'Palace of Fine Arts, San Francisco, California',
-  'Petronas Twin Towers, Kuala Lumpur, Malaysia',
   'Sunset at the Golden Gate Bridge, San Francisco, California',
+  'Golden Gate Bridge, San Francisco, California',
+  'Near to Alcatraz Island, San Francisco, California',
+  'A Boat Club, San Francisco, California',
+  'Petronas Twin Towers, Kuala Lumpur, Malaysia',
   'Late-evening view of the Island, Hong Kong',
 ];
 
 export default function PhotographyPage() {
   return (
     <InternalPostContainer>
-      <div className="grid grid-cols-1 gap-12">
+      <div className="grid grid-cols-1 gap-12 pb-12">
         {Array.from({ length: PHOTOGRAPHY_IMAGES }).map((_, index) => (
-          <div key={index} className="space-y-4">
+          <div key={index} className="flex flex-col gap-4 items-center w-full">
             <Image
               src={`/photography/${index + 1}.JPG`}
               alt={`Photography ${index + 1}`}
               width={1200}
               height={800}
-              className="select-none pointer-events-none w-full h-full object-contain"
+              className="select-none pointer-events-none w-full h-full object-cover"
             />
             <p className="text-sm text-center font-medium">
               {`${index + 1}. ${PHOTOGRAPHY_IMAGES_CAPTIONS[index]}`}
