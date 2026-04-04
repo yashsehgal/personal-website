02cb57a464cbd2eba1b94d2986a44ab957e9bd99
Yash Sehgal <yashsehgal.work@gmail.com>
2026-04-04 20:46:09 +0530

    feat: initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'github.com',
-        pathname: '/**',
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
