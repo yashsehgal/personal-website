8eebb4641b9fc3b17fc2016789bda8bc27ab5f33
Yash Sehgal <yashsehgal.work@gmail.com>
2026-04-05 00:42:12 +0530

    hide study for now

--- components/shared/navigation.tsx
+++ components/shared/navigation.tsx
@@ -7,7 +7,7 @@ const NAVIGATION_HIGHLIGHTED_LINKS: {
   title: string;
   route: ApplicationRoute;
 }[] = [
-  { title: "Study", route: ROUTES.STUDY },
+  // { title: "Study", route: ROUTES.STUDY },
   { title: "Writings", route: ROUTES.WRITINGS },
   { title: "About", route: ROUTES.ABOUT },
   { title: "Feed", route: ROUTES.FEED },
