61bc9aed67dfe92fe0fc2571d7d69fc85cf32477
Yash Sehgal <yashsehgal.work@gmail.com>
2026-04-13 14:47:03 +0530

    work in progress

--- features/personal-feed/components/feed-sidebar.tsx
+++ features/personal-feed/components/feed-sidebar.tsx
@@ -1,3 +1,8 @@
+"use client";
+import { useFeeds } from "@/hooks/use-feeds";
+
 export function FeedSidebar() {
+  const { data, isLoading } = useFeeds();
+
   return <aside className="w-1/5 p-2"></aside>;
 }
