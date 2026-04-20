bb95f8429d6c3c116c37efdcf4c94b85049a7073
Yash Sehgal <yashsehgal.work@gmail.com>
2026-04-21 05:22:20 +0530

    feed day date grouping container

---
+++ features/personal-feed/components/feed-messages-view/feed-messages-daydate-group.tsx
@@ -0,0 +1,27 @@
+import { formatDate } from "date-fns";
+import { useMemo } from "react";
+
+interface FeedMessagesDayDateGroupProps {
+  timestamp: Date;
+  children: React.ReactNode;
+}
+
+export function FeedMessagesDayDateGroup({
+  timestamp,
+  children,
+}: FeedMessagesDayDateGroupProps) {
+  const safeTimestamp = useMemo(() => {
+    return formatDate(timestamp, "MMM d, yyyy");
+  }, [timestamp]);
+
+  return (
+    <div className="border-t py-8 relative w-full h-fit">
+      <div className="absolute -top-4 right-1/2 translate-x-1/2 w-fit h-fit px-3 py-1 border border-border rounded-full bg-background">
+        <p className="text-xs text-muted-foreground font-medium">
+          {safeTimestamp}
+        </p>
+      </div>
+      {children}
+    </div>
+  );
+}
