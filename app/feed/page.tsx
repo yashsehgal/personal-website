44e19d796bb5289f2793fa8a0c901d1ea2c56065
Yash Sehgal <yashsehgal.work@gmail.com>
2026-04-26 09:08:34 +0530

    remove UI for testing feed CRUD operations

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,9 +1,5 @@
-import { FeedLayoutContainer } from "@/features/personal-feed/components/feed-layout-container";
-
 export default function FeedPage() {
   return (
-    <div className="flex min-h-0 w-full min-w-0 flex-1 flex-col overflow-hidden">
-      <FeedLayoutContainer />
-    </div>
+    <div className="flex min-h-0 w-full min-w-0 flex-1 flex-col overflow-hidden"></div>
   );
 }
