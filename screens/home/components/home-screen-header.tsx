50a7aa7769836436669ce4e39cf13263073d92e9
Yash Sehgal <yashsehgal.work@gmail.com>
2026-09-05 00:13:31 +0530

    Update HomeScreenHeader to change the font style from serif to medium, enhancing typography consistency across the component.

--- screens/home/components/home-screen-header.tsx
+++ screens/home/components/home-screen-header.tsx
@@ -1,7 +1,7 @@
 export function HomeScreenHeader() {
   return (
     <header>
-      <h1 className="text-2xl font-serif">Hi, I&apos;m Yash</h1>
+      <h1 className="text-2xl font-medium">Hi, I&apos;m Yash</h1>
     </header>
   );
 }
