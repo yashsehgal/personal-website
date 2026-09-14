312574a57b856b00c1cc4222b60432ef99764388
Yash Sehgal <yashsehgal.work@gmail.com>
2026-09-14 17:58:24 +0530

    Add HomeWorkExperienceListSection component and integrate it into the Home page; update global styles to include overscroll-none for improved user experience. Create a new Work page for future content expansion.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import { HomeIntroductionSection } from "@/modules/home-page/home-introduction-section";
+import { HomeWorkExperienceListSection } from "@/modules/home-page/home-work-experience-list-section";
 
 export default function Home() {
   return (
     <div>
       <div className="mt-20 mb-18">
         <HomeIntroductionSection />
+      </div>
+      <div>
+        <HomeWorkExperienceListSection />
       </div>
     </div>
   );
