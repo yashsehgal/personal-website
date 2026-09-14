b260ce2032d34e77f052a3ef3f3948cacda02027
Yash Sehgal <yashsehgal.work@gmail.com>
2026-09-14 18:26:31 +0530

    Add WorkHeaderSection component to Work page; update Work page layout to include new header section for improved content presentation.

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,3 +1,11 @@
+import { WorkHeaderSection } from "@/modules/work-page/work-header-section";
+
 export default function Work() {
-  return <div>Work</div>;
+  return (
+    <div>
+      <div className="mt-20 mb-18">
+        <WorkHeaderSection />
+      </div>
+    </div>
+  );
 }
---
+++ modules/work-page/work-header-section.tsx
@@ -0,0 +1,7 @@
+export function WorkHeaderSection() {
+  return (
+    <header>
+      <h1 className="font-semibold uppercase">Work</h1>
+    </header>
+  );
+}
