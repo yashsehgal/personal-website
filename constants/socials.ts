8cb60abeb1e9a9364eeb24e23b98f10191b9bc7d
Yash Sehgal <yashsehgal.work@gmail.com>
2026-09-21 06:35:02 +0530

    remove old website code

--- constants/socials.ts
+++
@@ -1,6 +0,0 @@
-export const SOCIALS = {
-  X: 'https://x.com/yashsehgaldev',
-  LINKEDIN: 'https://www.linkedin.com/in/sehgalyash/',
-  GITHUB: 'https://github.com/yashsehgal',
-  MAIL: 'yashsehgal.work@gmail.com',
-} as const;
