dbd17d470c9d5419c32e4aa2101d62db9ff03fa9
Yash Sehgal <yashsehgal.work@gmail.com>
2026-09-22 01:17:34 +0530

    Add logo image to sidebar navigation and create SVG asset

--- components/layouts/main-sidebar-navigation.tsx
+++ components/layouts/main-sidebar-navigation.tsx
@@ -2,6 +2,7 @@
 
 import { WEBSITE_ROUTES, WebsiteRouteType } from "@/common/routes";
 import { cn } from "cn";
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useCallback, useMemo } from "react";
@@ -37,6 +38,19 @@ export function MainSidebarNavigation() {
 
   return (
     <aside className="flex flex-col items-start gap-4">
+      <Image
+        src="/assets/initials.svg"
+        alt="Yash Sehgal"
+        width={100}
+        height={100}
+        className="size-10 select-none pointer-events-none dark:invert mb-6"
+        draggable={false}
+        priority
+        quality={100}
+        unoptimized
+        loading="eager"
+        fetchPriority="high"
+      />
       <header>
         <Link href={WEBSITE_ROUTES.HOME} className="size-fit block">
           <div
