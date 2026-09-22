15cd0b0ef1aea6b38b12d2d1159a5f2dcb83ea64
Yash Sehgal <yashsehgal.work@gmail.com>
2026-09-23 03:50:47 +0530

    Enhance sidebar navigation by adding hover effect classes to social links and improving layout styles for better user experience

--- components/layouts/main-sidebar-navigation.tsx
+++ components/layouts/main-sidebar-navigation.tsx
@@ -12,12 +12,36 @@ const NAVIGATION_ITEMS: { label: string; href: WebsiteRouteType }[] = [
   { label: "Photography", href: WEBSITE_ROUTES.PHOTOGRAPHY },
 ] as const;
 
-const SOCIAL_LINKS: { label: string; href: string }[] = [
-  { label: "X (Twitter)", href: "https://x.com/yashsehgaldev" },
-  { label: "GitHub", href: "https://github.com/yashsehgal" },
-  { label: "Instagram", href: "https://www.instagram.com/sehgalyash_/" },
-  { label: "LinkedIn", href: "https://www.linkedin.com/in/sehgalyash/" },
-  { label: "hi@yashsehgal.com", href: "mailto:hi@yashsehgal.com" },
+const SOCIAL_LINKS: {
+  label: string;
+  href: string;
+  overrideHoverClassname: string;
+}[] = [
+  {
+    label: "X (Twitter)",
+    href: "https://x.com/yashsehgaldev",
+    overrideHoverClassname: "hover:bg-foreground hover:text-background",
+  },
+  {
+    label: "GitHub",
+    href: "https://github.com/yashsehgal",
+    overrideHoverClassname: "hover:bg-foreground hover:text-background",
+  },
+  {
+    label: "Instagram",
+    href: "https://www.instagram.com/sehgalyash_/",
+    overrideHoverClassname: "hover:bg-pink-500 hover:text-white",
+  },
+  {
+    label: "LinkedIn",
+    href: "https://www.linkedin.com/in/sehgalyash/",
+    overrideHoverClassname: "hover:bg-sky-600 hover:text-white",
+  },
+  {
+    label: "hi@yashsehgal.com",
+    href: "mailto:hi@yashsehgal.com",
+    overrideHoverClassname: "hover:bg-rose-500 hover:text-white",
+  },
 ] as const;
 
 export function MainSidebarNavigation() {
@@ -37,7 +61,7 @@ export function MainSidebarNavigation() {
 
   return (
     <aside className="flex flex-col items-start gap-4 w-72 sticky top-8 shrink-0">
-      <header>
+      <header className="px-1">
         <Link href={WEBSITE_ROUTES.HOME} className="size-fit block">
           <div
             className={cn(
@@ -67,10 +91,10 @@ export function MainSidebarNavigation() {
               <Link
                 href={item.href}
                 className={cn(
-                  "text-sm tracking-tight font-medium",
+                  "text-sm tracking-tight font-medium select-none rounded px-1 py-0.5",
                   isNavigationItemActive(item.href)
-                    ? "text-foreground"
-                    : "text-muted-foreground hover:text-foreground",
+                    ? "text-foreground bg-muted"
+                    : "text-muted-foreground hover:bg-muted",
                 )}
               >
                 {item.label}
@@ -87,7 +111,10 @@ export function MainSidebarNavigation() {
                 target="_blank"
                 rel="noopener noreferrer"
                 href={link.href}
-                className="text-sm tracking-tight text-muted-foreground hover:text-foreground"
+                className={cn(
+                  "font-medium text-sm tracking-tight text-muted-foreground rounded px-1 py-0.5",
+                  link.overrideHoverClassname,
+                )}
               >
                 {link.label}
               </Link>
